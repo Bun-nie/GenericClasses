bb79db9cbdaef4265d3d482fef4300895aa38c6d
Bun-nie <nicoleejares28@gmail.com>
2024-03-26 17:52:07 +0800

    Made few changes in Arithmetic class

--- src/Arithmetic/Arithmetic.java
+++ src/Arithmetic/Arithmetic.java
@@ -1,30 +1,38 @@
 package Arithmetic;
 
-public class Arithmetic {
-    private Number number1, number2;
-    public Arithmetic(Number number1, Number number2) {
+public class Arithmetic<X extends Number, Y extends Number> {
+    private final X number1;
+    private final Y number2;
+
+    public Arithmetic(X number1, Y number2) {
         this.number1 = number1;
         this.number2 = number2;
     }
-    public Number add(){
+
+    public Number add() {
         return number1.doubleValue() + number2.doubleValue();
     }
-    public Number subtract(){
+
+    public Number subtract() {
         return number1.doubleValue() - number2.doubleValue();
     }
-    public Number multiply(){
+
+    public Number multiply() {
         return number1.doubleValue() * number2.doubleValue();
     }
-    public Number divide(){
-        if(number2.doubleValue() == 0){
+
+    public Number divide() {
+        if (number2.doubleValue() == 0) {
             throw new ArithmeticException("Math ERROR... Divisor cannot be zero!");
         }
         return number1.doubleValue() / number2.doubleValue();
     }
-    public Number getMin(){
+
+    public Number getMin() {
         return (number1.doubleValue() < number2.doubleValue() ? number1 : number2);
     }
-    public Number getMax(){
+
+    public Number getMax() {
         return (number1.doubleValue() < number2.doubleValue() ? number2 : number1);
     }
 }
